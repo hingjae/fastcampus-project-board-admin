a0f51fa352b7ca6529f92474b1c062dab5985d6a
hingjae <ljh23270@naver.com>
2023-06-14 09:49:33 +0900

    컨트롤러 설계, 테스트

---
+++ src/main/java/com/example/boardprojectadmin/controller/UserAccountManagementController.java
@@ -0,0 +1,9 @@
+package com.example.boardprojectadmin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/management/user-account")
+@Controller
+public class UserAccountManagementController {
+}
